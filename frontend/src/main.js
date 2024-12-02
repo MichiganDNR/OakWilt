645481b31ecd1d187228510329817a392e530a3a
usmanqt <qureshiu@mail.gvsu.edu>
2024-12-02 13:12:26 -0500

    Added a basic sign in screen as the default landing page, Installed firebase sdk for clientside and added firebase config file in the project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,19 @@ import Vue from "vue";
 import App from "./App.vue";
 import axios from "axios";
 import router from "./router";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import firebaseConfig from "../configs/firebase";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication
+const auth = getAuth(app);
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
+Vue.prototype.$auth = auth;
 
 new Vue({
   router,
